013fa69d754af1de6752023926ada796bb4571b6
Jacob <jacobkellogg17@gmail.com>
2023-08-03 20:12:38 -0500

    Finished code

---
+++ part1/studio/src/components/Photos.js
@@ -0,0 +1,7 @@
+import styles from './Description.module.css';
+
+let RecipePhoto = () => {
+    return <img src="https://www.allrecipes.com/thmb/8QzNWDvGhdry6V1jnyJWIhKA_nk=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20144-banana-banana-bread-mfs-57-0bb49d050a6941c4b3715e57b8e6badd.jpg" alt="recipe photo" className = {styles.imageUpdates} />;
+}
+
+export default RecipePhoto;
